a3093ecff16c8a4005915321b2be312022c94f27
alim-sokurov <60800545+alim-sokurov@users.noreply.github.com>
2020-02-08 02:52:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView:2,
+    loop: true,
+    navigation: {
+      nextEl: '.arrow',  
+    }
+});
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+  menuButton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})
